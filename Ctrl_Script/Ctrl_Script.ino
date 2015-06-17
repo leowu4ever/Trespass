e24ce75fd6895444485a3655a00ea084ff834fed
leowu4ever <wupopi@gmail.com>
2015-06-17 15:25:15 +0100

    First Commit
    Default folders setup.
    
    Trespass masterfolder contains Ctrl_Script folder (Arduino code) and
    Interface folder (Processing code).

---
+++ Ctrl_Script/Ctrl_Script.ino
@@ -0,0 +1,21 @@
+/*
+  Ctrl_Script 
+  Control one dance robot 
+ 
+  1 clutches & 3 brakes & 3 pressure pads ---------7 digital pins 
+  stepper motor (step & dirction) 2 digital pins
+  
+  by Leo Wu (King's College London)
+*/
+
+void setup() {
+  // put your setup code here, to run once:
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+
---
+++ README.md
@@ -0,0 +1,3 @@
+# Trespass
+Controls the robot(stepper motors, pressure pads, clutches and brakes). 
+Also build an interface for the robot 
